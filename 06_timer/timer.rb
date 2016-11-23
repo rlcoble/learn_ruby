8db1b2520c8d51d77bebb83d2b168b3478fd8b5f
Ryan Coble <ryan.coble@gmail.com>
2016-11-22 23:12:47 -0500

    made all tests pass

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,14 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  
+  def initialize
+  	@seconds=0
+  end
+
+  def time_string
+  	mm,ss=seconds.divmod(60)
+  	hh,mm=mm.divmod(60)
+  	t=[hh.to_s.rjust(2,'0'),mm.to_s.rjust(2,'0'),ss.to_s.rjust(2,'0')]
+  	t.join(":")
+  end
 end
